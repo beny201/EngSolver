5d476d41e759137fc5998bd713529356065c1f81
beny201 <artur.wudz@gmail.com>
2023-10-07 17:08:59 +0200

    feat: Adding new form for ridge connection

--- src/distance_checker/forms.py
+++ src/distance_checker/forms.py
@@ -41,11 +41,67 @@ class CornerForm(forms.Form):
         ("8_8", "8.8"),
         ("10_9", "10.9")
     )
-    bolt_grade = forms.ChoiceField(choices=choices_bolt,)
+    bolt_grade = forms.ChoiceField(choices=choices_bolt, )
 
     bolt_diameter = forms.ModelChoiceField(
-        queryset=Bolt.objects.values_list("diameter", flat=True).distinct().order_by("diameter"),
+        queryset=Bolt.objects.values_list("diameter",
+                                          flat=True).distinct().order_by(
+            "diameter"),
         to_field_name='diameter',
         initial=30
     )
 
+
+class RidgeForm(forms.Form):
+    left_girder_angle = forms.FloatField(
+        label="Girder angle [degree]",
+        min_value=1,
+        max_value=89,
+        initial=14.5
+    )
+
+    right_girder_angle = forms.FloatField(
+        label="Girder angle [degree]",
+        min_value=1,
+        max_value=89,
+        initial=14.5
+
+    )
+
+    girder_height = forms.IntegerField(
+        label="Girder height [mm]",
+        min_value=1,
+        initial=400
+    )
+    left_t_flange_girder = forms.IntegerField(
+        label="Thickness of flange in girder [mm]",
+        min_value=1,
+        max_value=120,
+        initial=20
+    )
+
+    right_t_flange_girder = forms.IntegerField(
+        label="Thickness of flange in girder [mm]",
+        min_value=1,
+        max_value=120,
+        initial=20
+    )
+
+    t_plate_connection = forms.IntegerField(
+        label="Thickness of plate in connection [mm]",
+        min_value=1,
+        initial=20
+    )
+    choices_bolt = (
+        ("8_8", "8.8"),
+        ("10_9", "10.9")
+    )
+    bolt_grade = forms.ChoiceField(choices=choices_bolt, )
+
+    bolt_diameter = forms.ModelChoiceField(
+        queryset=Bolt.objects.values_list("diameter",
+                                          flat=True).distinct().order_by(
+            "diameter"),
+        to_field_name='diameter',
+        initial=30
+    )
