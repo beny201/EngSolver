c5be81fb7b4dca50a81bee9b3058f55e6d04fcad
beny201 <artur.wudz@gmail.com>
2023-10-28 10:42:26 +0200

    refactor: Creating model for ridge, creating form from model

--- src/distance_checker/forms.py
+++ src/distance_checker/forms.py
@@ -1,61 +1,15 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import Bolt, Corner
+from .models import Bolt, Corner, Ridge
 
-# class CornerForm(forms.Form):
-#     girder_angle = forms.FloatField(
-#         label="Girder angle [degree]",
-#         min_value=1,
-#         max_value=89,
-#         initial=14.5
-#     )
-#     girder_height = forms.IntegerField(
-#         label="Girder height [mm]",
-#         min_value=1,
-#         initial=400
-#     )
-#     t_flange_girder = forms.IntegerField(
-#         label="Thickness of flange in girder [mm]",
-#         min_value=1,
-#         max_value=120,
-#         initial=20
-#     )
-#     column_width = forms.IntegerField(
-#         label="Column width [mm]",
-#         min_value=1,
-#         max_value=2000,
-#         initial=900
-#     )
-#     t_flange_column = forms.IntegerField(
-#         label="Thickness of flange in column [mm]",
-#         min_value=1,
-#         max_value=120,
-#         initial=20
-#     )
-#     t_plate_connection = forms.IntegerField(
-#         label="Thickness of plate in connection [mm]",
-#         min_value=1,
-#         initial=20
-#     )
-#     choices_bolt = (
-#         ("8_8", "8.8"),
-#         ("10_9", "10.9")
-#     )
-#     bolt_grade = forms.ChoiceField(choices=choices_bolt, )
-#
-#     bolt_diameter = forms.ModelChoiceField(
-#         queryset=Bolt.objects.values_list("diameter",
-#                                           flat=True).distinct().order_by(
-#             "diameter"),
-#         to_field_name='diameter',
-#         initial=30
-#     )
+searched_bolt_standard = "14399-4"
 
 
 class CornerFormModel(ModelForm):
     bolt_diameter = forms.ModelChoiceField(
         queryset=Bolt.objects.values_list("diameter", flat=True)
+        .filter(standard__title=searched_bolt_standard)
         .distinct()
         .order_by("diameter"),
         to_field_name='diameter',
@@ -85,42 +39,12 @@ class CornerFormModel(ModelForm):
         }
 
         widgets = {
-            'girder_angle': forms.NumberInput(
-                attrs={
-                    'max': '89',
-                    'min': '0.1',
-                }
-            ),
-            'girder_height': forms.NumberInput(
-                attrs={
-                    'max': '2000',
-                    'min': '100',
-                }
-            ),
-            't_flange_girder': forms.NumberInput(
-                attrs={
-                    'max': '100',
-                    'min': '5',
-                }
-            ),
-            'column_width': forms.NumberInput(
-                attrs={
-                    'max': '2500',
-                    'min': '100',
-                }
-            ),
-            't_flange_column': forms.NumberInput(
-                attrs={
-                    'max': '100',
-                    'min': '5',
-                }
-            ),
-            't_plate_connection': forms.NumberInput(
-                attrs={
-                    'max': '150',
-                    'min': '5',
-                }
-            ),
+            'girder_angle': forms.NumberInput(attrs={'max': '89', 'min': '0'}),
+            'girder_height': forms.NumberInput(attrs={'max': '2000', 'min': '100'}),
+            't_flange_girder': forms.NumberInput(attrs={'max': '100', 'min': '5'}),
+            'column_width': forms.NumberInput(attrs={'max': '2500', 'min': '100'}),
+            't_flange_column': forms.NumberInput(attrs={'max': '100', 'min': '5'}),
+            't_plate_connection': forms.NumberInput(attrs={'max': '150', 'min': '5'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -136,44 +60,58 @@ class CornerFormModel(ModelForm):
         self.fields['bolt_diameter'].initial = 30
 
 
-class RidgeForm(forms.Form):
-    left_girder_angle = forms.FloatField(
-        label="Left girder angle [degree]", min_value=1, max_value=89, initial=14.5
-    )
-
-    right_girder_angle = forms.FloatField(
-        label="Right girder angle [degree]", min_value=1, max_value=89, initial=14.5
-    )
-
-    girder_height = forms.IntegerField(
-        label="Girder height [mm]", min_value=1, initial=400
-    )
-    left_t_flange_girder = forms.IntegerField(
-        label="Thickness of left flange in girder [mm]",
-        min_value=1,
-        max_value=120,
-        initial=20,
-    )
-
-    right_t_flange_girder = forms.IntegerField(
-        label="Thickness of right flange in girder [mm]",
-        min_value=1,
-        max_value=120,
-        initial=20,
-    )
-
-    t_plate_connection = forms.IntegerField(
-        label="Thickness of plate in connection [mm]", min_value=1, initial=20
-    )
-    choices_bolt = (("8_8", "8.8"), ("10_9", "10.9"))
-    bolt_grade = forms.ChoiceField(
-        choices=choices_bolt,
-    )
-
+class RidgeFormModel(ModelForm):
     bolt_diameter = forms.ModelChoiceField(
         queryset=Bolt.objects.values_list("diameter", flat=True)
+        .filter(standard__title=searched_bolt_standard)
         .distinct()
         .order_by("diameter"),
         to_field_name='diameter',
-        initial=30,
     )
+
+    class Meta:
+        model = Ridge
+        fields = [
+            'case',
+            'left_girder_angle',
+            'right_girder_angle',
+            'girder_height',
+            'left_t_flange_girder',
+            'right_t_flange_girder',
+            't_plate_connection',
+            'bolt_grade',
+            'bolt_diameter',
+        ]
+
+        labels = {
+            'case': 'Name or number of case ',
+            'left_girder_angle': 'Left girder angle [degree]',
+            'right_girder_angle': "Right girder angle [degree]",
+            'girder_height': 'Girder height [mm]',
+            'left_t_flange_girder': 'Thickness of left flange in girder [mm]',
+            'right_t_flange_girder': 'Thickness of right flange in girder [mm]',
+            't_plate_connection': 'Thickness of plate in connection [mm]',
+        }
+
+        widgets = {
+            'left_girder_angle': forms.NumberInput(attrs={'max': '89', 'min': '0'}),
+            'right_girder_angle': forms.NumberInput(attrs={'max': '89', 'min': '0'}),
+            'girder_height': forms.NumberInput(attrs={'max': '2000', 'min': '100'}),
+            'left_t_flange_girder': forms.NumberInput(attrs={'max': '100', 'min': '5'}),
+            'right_t_flange_girder': forms.NumberInput(
+                attrs={'max': '100', 'min': '5'}
+            ),
+            't_plate_connection': forms.NumberInput(attrs={'max': '150', 'min': '5'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['case'].initial = 1000
+        self.fields['left_girder_angle'].initial = 14.5
+        self.fields['right_girder_angle'].initial = 14.5
+        self.fields['girder_height'].initial = 400
+        self.fields['left_t_flange_girder'].initial = 20
+        self.fields['right_t_flange_girder'].initial = 20
+        self.fields['t_plate_connection'].initial = 20
+        self.fields['bolt_grade'].initial = "8_8"
+        self.fields['bolt_diameter'].initial = 30
--- src/distance_checker/models.py
+++ src/distance_checker/models.py
@@ -59,18 +59,14 @@ class Nut(models.Model):
 
 
 class Corner(models.Model):
-    case = models.CharField(
-        max_length=50,
-    )
+    case = models.CharField(max_length=50)
     girder_angle = models.FloatField()
     girder_height = models.IntegerField()
     t_flange_girder = models.IntegerField()
     column_width = models.IntegerField()
     t_flange_column = models.IntegerField()
     t_plate_connection = models.IntegerField()
-    bolt_grade = models.TextField(
-        choices=[("8_8", "8.8"), ("10_9", "10.9")],
-    )
+    bolt_grade = models.TextField(choices=[("8_8", "8.8"), ("10_9", "10.9")])
     bolt_diameter = models.IntegerField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     last_modified_date = models.DateTimeField(auto_now=True)
@@ -80,4 +76,25 @@ class Corner(models.Model):
 
     def __str__(self):
         title = f"Corner - {self.case} - {self.created_date}"
+        return title
+
+
+class Ridge(models.Model):
+    case = models.CharField(max_length=50)
+    left_girder_angle = models.FloatField()
+    right_girder_angle = models.FloatField()
+    girder_height = models.IntegerField()
+    left_t_flange_girder = models.IntegerField()
+    right_t_flange_girder = models.IntegerField()
+    t_plate_connection = models.IntegerField()
+    bolt_grade = models.TextField(choices=[("8_8", "8.8"), ("10_9", "10.9")])
+    bolt_diameter = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    distance_top = models.IntegerField(null=True)
+    distance_bottom = models.IntegerField(null=True)
+
+    def __str__(self):
+        title = f"Ridge - {self.case} - {self.created_date}"
         return title
