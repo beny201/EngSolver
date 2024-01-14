7bfb12205723889edb29247d7dbb2618b12c210d
beny201 <artur.wudz@gmail.com>
2024-01-14 10:51:13 +0100

    Refactor: Removed duplicated 'evaluate' function from test classes, creating one common in EngSolver main folder

--- distance_checker/tests/test_corner.py
+++ distance_checker/tests/test_corner.py
@@ -1,16 +1,10 @@
-from django.test import TestCase, tag
+from django.test import TestCase
 
 from distance_checker.calculation import CreatingCorner
+from EngSolver.test import evaluate
 
 
 class CreatingCornerTestCase(TestCase):
-    @staticmethod
-    def evaluate(value):
-        ev_1 = value * 0.99
-        ev_2 = value * 1.01
-
-        return ev_2 - ev_1
-
     def test_should_return_top_and_bottom_value_for_equal_parts(self):
         tested = CreatingCorner()
         tested_value = tested.creating_lines(
@@ -25,11 +19,10 @@ class CreatingCornerTestCase(TestCase):
         )
         tested_value_bottom, tested_value_top = tested_value[1]
         expected = 128
-        delta = self.evaluate(expected)
+        delta = evaluate(expected)
         self.assertAlmostEqual(tested_value_bottom, expected, delta=delta)
         self.assertAlmostEqual(tested_value_top, expected, delta=delta)
 
-    @tag("x")
     def test_should_return_top_and_bottom_value_for_not_equal_parts(self):
         tested = CreatingCorner()
         tested_value = tested.creating_lines(
@@ -43,10 +36,9 @@ class CreatingCornerTestCase(TestCase):
             space_for_screw=40,
         )
         tested_value_bottom, tested_value_top = tested_value[1]
-        print(tested_value)
         expected_top = 218
         expected_bottom = 133
-        delta_top = self.evaluate(expected_top)
-        delta_bottom = self.evaluate(expected_bottom)
+        delta_top = evaluate(expected_top)
+        delta_bottom = evaluate(expected_bottom)
         self.assertAlmostEqual(tested_value_bottom, expected_bottom, delta=delta_bottom)
         self.assertAlmostEqual(tested_value_top, expected_top, delta=delta_top)
